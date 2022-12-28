fa68f6ce0d5b6e33cc288546471798dcca1b6a12
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-28 21:15:21 +0530

    Update p4.dart

--- Lab 1/p4.dart
+++ Lab 1/p4.dart
@@ -3,15 +3,15 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  print("Enter Marks : ");
+  print("Enter Marks of Sub1 : ");
   var sub11 = int.parse(stdin.readLineSync()!);
-  print("Enter Marks : ");
+  print("Enter Marks of sub2 : ");
   var sub2 = int.parse(stdin.readLineSync()!);
-  print("Enter Marks : ");
+  print("Enter Marks of sub3 : ");
   var sub3 = int.parse(stdin.readLineSync()!);
-  print("Enter Marks : ");
+  print("Enter Marks of sub4 : ");
   var sub4 = int.parse(stdin.readLineSync()!);
-  print("Enter Marks : ");
+  print("Enter Marks of sub5 : ");
   var sub5 = int.parse(stdin.readLineSync()!);
   var per = (sub1 + sub2 + sub3 + sub4 + sub5) / 5;
   print("percentage : $per");
