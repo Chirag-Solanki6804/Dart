7f2870976b59f2c0c7c542a20b93737f046799a6
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-28 21:16:05 +0530

    Update p2.dart

--- Lab 1/p2.dart
+++ Lab 1/p2.dart
@@ -1,7 +1,7 @@
 // Write a dart code to print the addition of 2 numbers.
 
 void main(List<String> args) {
-  var a = 10;
-  var b = 20;
-  print("Addition : ${a+b}");
+  var num1 = 10;
+  var num2 = 20;
+  print("Addition : ${num1+num2}");
 }
