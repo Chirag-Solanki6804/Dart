96c2b78c67146c8bcb26c5ede54fc108d4b2d768
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-12-28 21:48:37 +0530

    Update p6.dart

--- Lab 2/p6.dart
+++ Lab 2/p6.dart
@@ -4,28 +4,28 @@ import 'dart:io';
 
 void main(List<String> args) {
   print("Enter First Number : ");
-  int a = int.parse(stdin.readLineSync()!);
+  int num1 = int.parse(stdin.readLineSync()!);
   print("Enter First Number : ");
-  int b = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!);
   int choice=0;
   while(choice!=5){
   print("Enter 1 to Perform Addition\nEnter 2 to Perform Subtraction\nEnter 3 to Perform Multiplication\nEnter 4 to Perform Division\nEnter 5 to Exit program\n");
   choice = int.parse(stdin.readLineSync()!);
    switch (choice) {
     case 1:
-      int addition = a + b;
+      int addition = num1 + num2;
       print("Addition : $addition");
       break;
     case 2:
-      int subtraction = a - b;
+      int subtraction = num1 - num2;
       print("Sutraction : $subtraction");
       break;
     case 3:
-      int multiplication = a * b;
+      int multiplication = num1 * num2;
       print("Multiplication : $multiplication");
       break;
     case 4:
-      double division = a / b;
+      double division = num1 / num2;
       print("Division : $division");
       break;
     case 5:
